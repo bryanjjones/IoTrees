f07674f2a4c41064a2c6aa3de515164b3b79460e
Bryan Jones <bryanjjones@gmail.com>
2017-10-28 23:50:19 +0000

    cleaned out unused lines of code

--- map.py
+++ map.py
@@ -14,6 +14,16 @@ args = vars(ap.parse_args())
 
 json_data = json.loads(args['data'])
 
+orangelats=[]
+orangelngs=[]
+redlats=[]
+redlngs=[]
+yellowlats=[]
+yellowlngs=[]
+greenlats=[]
+greenlngs=[]
+
+#sample data
 orangelats=[44.985563,44.970366,44.992770]
 orangelngs=[-93.182058,-93.177255,-93.186360]
 redlats=[44.983365,44.998281,44.996532]
@@ -22,6 +32,19 @@ yellowlats=[44.956277,44.968683,44.977585]
 yellowlngs=[-93.171469,-93.153370,-93.164273]
 greenlats=[44.988368,44.968263,44.957578]
 greenlngs=[-93.163870,-93.162276,-93.153360]
+
+if json_data.cout < green:
+    greenlats.append(json_data.lat)
+    greenlngs.append(json_data.lng)
+elif json_data.count < yellow:
+    yellowlats.append(json_data.lat)
+    yellowlngs.append(json_data.lng)
+elif json_data.count < orange: 
+    orangelats.append(json_data.lat)
+    orangelngs.append(json_data.lng)
+else:
+    redlats.append(json_data.lat)
+    redlngs.append(json_data.lng)
 '''
 for i in json_data:
     if i.count < green:
@@ -38,16 +61,9 @@ for i in json_data:
         redlngs.append(i.lng)
 '''
 gmap = _gmplot.GoogleMapPlotter(44.975473, -93.175166, 12)
-
-
-
-#gmap.plot([37.428], [-122.144], 'cornflowerblue', edge_width=10)
-#gmap.scatter(lats, lons, '#3B0B39', size=40, marker=False)
 gmap.scatter(orangelats, orangelngs, 'orange', marker=True)
 gmap.scatter(redlats, redlngs, 'red', marker=True)
 gmap.scatter(greenlats, greenlngs, 'green', marker=True)
 gmap.scatter(yellowlats, yellowlngs, 'yellow', marker=True)
-#gmap.scatter(lats, lons, 'k', marker=True)
-#gmap.heatmap(heat_lats, heat_lngs)
 
-gmap.draw("mymap.html")
+gmap.draw("IoTrees.html")
